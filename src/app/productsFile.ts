bdca07ec0b455a5bd7bffa8202f71ec1f612611f
Devadath-Samji <devadathsamji@gmail.com>
2020-12-13 21:57:03 +0530

    5 files modified

--- src/app/productsFile.ts
+++ src/app/productsFile.ts
@@ -1,16 +1,16 @@
 export const productsArray = [
   {
-    name: 'Phone XL',
+    name: 'Course 1',
     price: 799,
     description: 'A large phone with one of the best screens'
   },
   {
-    name: 'Phone Mini',
+    name: 'Course 2',
     price: 1699,
     description: 'A great phone with one of the best cameras'
   },
   {
-    name: 'Phone Standard',
+    name: 'Course 3',
     price: 299,
     description: ''
   }
